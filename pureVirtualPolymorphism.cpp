f5019a9c948bacc8a149a1881ab0131043c8dcbe
Anurag-0himire <anurag.ghimire21@gmail.com>
2026-08-18 10:40:26 +0545

    Add/update pureVirtualPolymorphism.cpp via C++ Quick Saver

---
+++ pureVirtualPolymorphism.cpp
@@ -0,0 +1,33 @@
+//pure virtual polymorphism
+#include<iostream>
+using namespace std;
+class Student{
+    public:
+        virtual void display(){
+            cout<<"I am a student."<<endl;
+        }
+};
+class Engineering:public Student{
+    public:
+        void display(){
+            cout<<"I am an Engineer Student"<<endl;
+        }
+};
+class Management:public Student{
+    public:
+        void display(){
+            cout<<"I am a Management Student"<<endl;
+        }
+};
+int main(){
+    Student *S;
+    Engineering E;
+    Management M;
+
+    S = &E;
+    S -> display();
+
+    S = &M;
+    S -> display();
+    return 0;
+}
